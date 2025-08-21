f19cf50a1ef11ce3a16ed41265ac819467f1477b
juentz <julie_entzian@hotmail.de>
2025-08-21 08:36:52 +0200

    First setup

---
+++ julia/test.jl
@@ -0,0 +1,11 @@
+# this is a test for later and should here I test how I can use julia for computing the stability of a food web matrix by computing its jacobian 
+
+function myadd(z, y)
+    x + y
+end
+#OR
+myadd2(z, y) = z+y
+
+println("everything works as it should - nice!")
+z=myadd(1, 2)
+println("z")
